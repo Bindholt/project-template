a7b490e33ae83440a8817bfba6e04eddcba7ac79
Bindholt <Cbindholt@gmail.com>
2023-01-31 13:47:41 +0100

    Har ryddet op i koden

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 console.log("app.js is running 🎉");
 
 function sayHello() {
-  document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+  let h3 = document.querySelector("h3");
+  let inputName = document.querySelector("#name").value;
+  let inputAge = document.querySelector("#age").value;
+  let inputEmail = document.querySelector("#email").value;
+
+  h3.textContent = "";
+
+  inputName ? (h3.textContent += "Hej " + inputName + ".") : "";
+
+  inputAge ? (h3.textContent += " Din alder er " + inputAge + ".") : "";
+
+  inputEmail ? (h3.textContent += " Din E-mail er " + inputEmail + ".") : "";
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
-document.querySelector("input").addEventListener("keyup", function (event) {
+document.querySelector("#name").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    sayHello();
+  }
+});
+document.querySelector("#age").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    sayHello();
+  }
+});
+document.querySelector("#email").addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     sayHello();
   }
