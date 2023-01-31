75b28b26bd42be873b7b74312291b058405bbea7
Bindholt <Cbindholt@gmail.com>
2023-01-31 12:25:44 +0100

    Har tilføjet sayHello() funktionen, og smidt eventlistener "click" på button og eventlistener "keyup" med if sætning på input.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("app.js is running 🎉");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("input").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    sayHello();
+  }
+});
